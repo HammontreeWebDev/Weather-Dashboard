0db96020946f1c96ae13413154252bbf8c8a8a80
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-09 00:20:10 -0400

    added some styling to the buttons.. added two more search fields for state and country.. will need to pass those parameters through the geo api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 // declare any global var associated with ID's and elements
 let citySearchID = $("#city-search")
 let tableBody = $("#weather-table");
+let searchBtn = $("#searchBtn");
 
 const app = {
     init: () => {
+        // add event listener if user presses the enter key
         citySearchID.on('keydown', (event)=> {
             if (event.keyCode === 13) {
                 app.fetchLocation();
             }
         });
+        // add event listener if user presses the search button
+        searchBtn.on('click', app.fetchLocation);
+
     },
     fetchLocation: (event) => {
         let cityName = $('input:text').val();
@@ -22,19 +27,37 @@ const app = {
             return response.json();
         })
         .then(geoData => {
-            app.showLocation(geoData);
+            app.fetchWeather(geoData);
         })
         .catch(console.error)
     },
-    showLocation: (response) => {
+    // take lat and long from user entered city name and pass them into a variable that will then be passed into a template literal string for the one call url so that we can get the weather without having to put in a lon and lat ourselves!!! WOOOHOOO.. Makin it easy for the user.
+    fetchWeather: (response) => {
         console.log(response)
         // works!!
         console.log(response[0].lat);
+        console.log(response[0].lon);
         let lat = response[0].lat;
         let lon = response[0].lon;
+        let key = '2c8438a889150a71aa165db59d155f28';
+        let oneCallUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`;
+
+        fetch(oneCallUrl)
+        .then(response => {
+            if (!response.ok) throw new Error(response.statusText)
+            return response.json();
+        })
+        .then(weatherData => {
+            app.showWeather(weatherData);
+        })
+        .catch(console.error)
+    },
+    showWeather: (response) => {
+        console.log(response);
     }
 }
 
+// initialize the page upon loading
 app.init();
 
 
